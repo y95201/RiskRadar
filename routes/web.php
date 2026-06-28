65e6c7d5c2039e6276a84b149fb027b6e1e37b81
y9520 <957612196@qq.com>
2026-06-29 04:13:46 +0800

    提交1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
+/*
+ * @Description: 
+ * @Author: Y95201
+ * @Date: 2026-05-26 07:11:52
+ * @LastEditors: Y95201
+ * @LastEditTime: 2026-06-29 04:13:00
+ */
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/video-generator', function () {
+    return view('dome');
 });
